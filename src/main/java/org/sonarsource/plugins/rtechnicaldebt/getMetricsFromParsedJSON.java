2269368a6b95d016dbcd99726bc16bf29f256548
pranav.chndramouli <pranav.chndramouli@gmail.com>
2022-01-09 13:12:37 -0600

    Documented version conflict issues. getMetricsFromParsedJSON.java - Fixed startup crashes.

--- src/main/java/org/sonarsource/plugins/rtechnicaldebt/getMetricsFromParsedJSON.java
+++ src/main/java/org/sonarsource/plugins/rtechnicaldebt/getMetricsFromParsedJSON.java
@@ -7,7 +7,7 @@ import java.io.IOException;
 import java.io.Reader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.List;
+import java.util.*;
 
 public class getMetricsFromParsedJSON {
 
@@ -16,6 +16,15 @@ public class getMetricsFromParsedJSON {
 
     public getMetricsFromParsedJSON() throws IOException {
         // this.metrics = getMetrics(JSON);
+        System.out.println(this);
+        String currentPath = new java.io.File(".").getCanonicalPath();
+        System.out.println("Current dir:" + currentPath);
+
+        String currentDir = System.getProperty("user.dir");
+        System.out.println("Current dir using System:" + currentDir);
+
+        metrics = new ArrayList<TechnicalDebtMetric>();
+        // TODO - Make sensor use this. Causes Null pointer exception, so this needs to stay till fixed.
     }
 
     private List<TechnicalDebtMetric> getMetrics(String filename) throws IOException {
