0e76a6ff5e0a0c0823bc48020067809204159cf8
pranav.chndramouli <9011488+pranavchandramouli@users.noreply.github.com>
2022-01-30 15:09:18 -0600

    Started updated sonar-scanner.sh

--- Script/sonar-scanner.sh
+++ Script/sonar-scanner.sh
@@ -3,13 +3,16 @@ SONARSCANNERDIR="$HOME/sonar-scanner"
 
 
 PWD="$(PWD)"
+PROJECTPATH="$1"
 echo "Your SonarQube Scanner is installed in: $SONARSCANNERDIR"
 echo "Please change this in the script, if this is incorrect."
-echo "The current working directory: $PWD and contains the following R files:"
-echo "$(find . -type f -name "*.R")"
+echo "The current working directory: $PROJECTPATH and contains the following R files:"
+echo "$(find . -type f -name "${PROJECTPATH}/*.R")"
+
+echo "Running the R-Scanner for R files in $PROJECTPATH"
+echo "${PROJECTPATH}/*.R"
+
+python3 RScanner.py "${PROJECTPATH}/*.R"
+
 echo "Running Sonar-Scanner now."
-
-python3 RScanner.py ./*.R
-
-
-"${SONARSCANNERDIR}/bin/sonar-scanner" "-Dsonar.login=admin -Dsonar.password=admin22"
+# "${SONARSCANNERDIR}/bin/sonar-scanner" "-Dsonar.login=admin -Dsonar.password=admin22"
