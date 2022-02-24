7baa190f0c6c4e94cecc49ed60c9645cdd9d639e
pranav.chandramouli <pranav.chandramouli@gmail.com>
2022-02-24 09:56:40 -0600

    Added documentation for RParse.R

--- Parsing-Scripts/RParse.R
+++ Parsing-Scripts/RParse.R
@@ -1,16 +1,15 @@
-
-#
-# Generates  R PARSE Tree in XML (using in built in Parser)
-# First Step in the R metrics generation 
-
-#  TO DO 
-# - Add Debug/ logs in separate file ?
-# 
-# Check if file exists 
-# check for other error conditions 
-
-
 library(xmlparsedata)
+
+#' Generates a R Parse Tree as an XML R PARSE Tree in XML using "xmlparsedata"
+#' Reads R source files, and saves it to a xml source file
+#'
+#' @importFrom xmlparsedata xml_parse_data
+#' @param srcfile source R file
+#' @param dstfile destination XML file, where the parse tree is stored.
+#' @author Pranav Chandramouli, University of Saskatchewan Supervised by Dr. Melina Vidoni and Dr. Zadia Codabux
+#' @return None
+#' @examples
+#' getxml(example.R,example.xml)
 getxml <- function(srcfile,dstfile) {
     txt<-readLines(srcfile)
     print(paste("Parsing R file ",srcfile))
@@ -20,15 +19,14 @@ getxml <- function(srcfile,dstfile) {
     writeLines(xml_text,dstfile)
 }
 
-
+#' Read command line arguments
 args = commandArgs(trailingOnly=TRUE)
+#' Check for invalid command line arguments, and if not, call getxml
 if (length(args) !=2) {
   stop("Rscript getxml.R <src.R> <dst.xml>", call.=FALSE)
 } else  {
   # default output file
   src<-args[1]
   dst<-args[2]
-
 }
-#getxml("src/s3class1.R","xml/s3class1.xml")
 getxml(src,dst)
