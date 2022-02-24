bb5ec616984c7049af3de502ffefe1eebfbe9a1c
pranav.chandramouli <pranav.chandramouli@gmail.com>
2022-02-24 17:30:50 -0600

    Fixed JavaDocs

--- src/main/java/org/sonarsource/plugins/rtechnicaldebt/measures/cumulative/ComputeAverageAMC.java
+++ src/main/java/org/sonarsource/plugins/rtechnicaldebt/measures/cumulative/ComputeAverageAMC.java
@@ -1,9 +1,3 @@
-/**
- * @author Pranav Chandramouli, University of Saskatchewan
- * Supervised by Dr. Melina Vidoni and Dr. Zadia Codabux
- */
-
-
 package org.sonarsource.plugins.rtechnicaldebt.measures.cumulative;
 
 import org.sonar.api.ce.measure.Component;
@@ -14,9 +8,10 @@ import org.sonar.api.ce.measure.MeasureComputer;
 import static org.sonarsource.plugins.rtechnicaldebt.measures.RMetrics.AVERAGE_METHOD_COMPLEXITY;
 
 /**
- * Computes Cumulative Average Method Complexity
+ * Measure Computer to compute Cumulative Average Method Complexity
+ * @author Pranav Chandramouli, University of Saskatchewan
+ * Supervised by Dr. Melina Vidoni and Dr. Zadia Codabux
  */
-
 public class ComputeAverageAMC implements MeasureComputer {
 
   /**
@@ -37,8 +32,7 @@ public class ComputeAverageAMC implements MeasureComputer {
    */
   @Override
   public void compute(MeasureComputerContext context) {
-    // measure is already defined on files by {@link SetSizeOnFilesSensor}
-    // in scanner stack
+
     if (context.getComponent().getType() != Component.Type.FILE) {
       double sum = 0;
       int count = 0;
